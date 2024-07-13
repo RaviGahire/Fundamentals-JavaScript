3861ea31d8c84e3696763098a715612c1667953e
RaviGahire <ravigahire3@gmail.com>
2024-07-13 19:58:33 +0530

    How to declare variables and rules of variables

---
+++ Variables/variables.js
@@ -0,0 +1,11 @@
+
+// using string data type 
+// varibale keyword  location = value ;
+let myName = "ravi";
+
+// using number data type
+let age = 28;
+// using boolean data type 
+let isHappy = true;
+
+const myIntro = 'Hi , i am' + ' '+ myName + ' '+'and my age is' + ' ' +age + ' '+ 'and i am boy'+ ' ' +isHappy
