d9203e37fb2d6987ef40fafbd93be97a5b8eb894
RaviGahire <ravigahire3@gmail.com>
2024-07-14 12:41:54 +0530

    Code execution in js

---
+++ Code Execution/file.js
@@ -0,0 +1,11 @@
+console.log("code execution")
+
+var firtsName = "ravi"
+var lastName = "gahire"
+
+let age = 28;
+const DOB = 1994 ;
+
+let myIntro = 'Hi, My name is ' +firtsName +' '+lastName
+
+console.log(myIntro)
