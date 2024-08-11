994309e859d80e80d252efef097fbf1c99d71c29
RaviGahire <ravigahire3@gmail.com>
2024-08-11 20:15:44 +0530

    Math Object

--- Math Object In Js/Notes.md
+++ Math Object In Js/Notes.md
@@ -1,1 +1,25 @@
-# Math object In javaScript 
+# Math object In javaScript.
+
+
+# Math in Javascript.
+
+    1. Addition operator ---> (+)
+    2. Subtraction operator ---> (-)
+    3. Multiplication operator ---> (*)
+    4. Division operator ---> (/)
+    5. Remainder operator ---> (%) New 
+    6. Exponentiation operator --->(**) New
+
+# Properties on math object 
+ 1. Math.PI 
+ 2. Math.SQRT2
+ 3. MAth.E
+
+ # Methods on math object 
+ 1. Math.sqrt(value) --> for square root. 
+ 2. Math.pow(value-1 , value-2) --> for power. 
+ 3. Math.floor(value) --> Math.floor() only remove the decimal numbers given value.
+ 4. Math.round(value)--> used for round the decimal value.
+ 5. Math.ceil(value)---> stretch the decimal number value to closet number.
+ 6. 
+
---
+++ Math Object In Js/math.js
@@ -0,0 +1,43 @@
+// Remainder operator example 
+let Remainder_Value_1= 11
+let Remainder_Value_2 = 2
+ 
+remainder = Remainder_Value_1 % Remainder_Value_2 
+
+console.log( "Remainder Value is-->", remainder )
+
+//Exponentiation operator calculate the the power of given numbers.
+
+
+ let Exponentiation_Value_1 = 5 
+ let Exponentiation_Value_2 = 2
+
+ result = Exponentiation_Value_1 ** Exponentiation_Value_2
+ 
+ console.log( "Exponentiation Value is-->" , result);
+
+ // Old method to calculate power 
+ console.log("This old method for calculate power in Js " , Math.pow( 2 , 4))
+
+// we can use this sqrt function for square-root 
+    console.log(Math.sqrt(16))
+
+// Math.floor this is just remove decimal value after given number 
+
+let floor_value = 22.225
+ 
+console.log("Math.floor Example ", Math.floor(floor_value));
+
+
+// Math.round this is used for round the given number 
+ 
+
+let roundValue = 5.99
+
+console.log("Math.round example--->" , Math.round(roundValue))
+
+// Math.ceil This is used for next closest number after decimal see the example for better understanding 
+
+let ceilValue = 2.01
+
+console.log("Math.ceil Example --->" ,Math.ceil(ceilValue))
