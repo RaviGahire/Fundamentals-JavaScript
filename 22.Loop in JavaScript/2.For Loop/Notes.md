5b7dea2d7b586934b085d6109b9e224dd5ddde22
unknown <ravigahire3@gmail.com>
2024-09-28 12:54:00 +0530

    For Loop

---
+++ 22.Loop in JavaScript/2.For Loop/Notes.md
@@ -0,0 +1,12 @@
+# For loop
+
+The for loop is same as while loop
+but syntax is diffrenet 
+
+syntax for (define variable ; conditon ; evaulate){
+    clg()
+}
+ Example 
+ for (let i = 0; i <= 5 ; i++){
+    console.log(i )
+}
---
+++ 22.Loop in JavaScript/2.For Loop/index.js
@@ -0,0 +1,17 @@
+
+
+// for (let i = 0; i <= 5 ; i++){
+//     console.log(i )
+// }
+
+// Assinment craete array and print modify do all the task on the array
+
+
+// Printing even number 
+
+for (let i = 0; i <= 100;i++){
+    if(i % 2 === 0){ 
+        console.log(i)
+    }
+}
+// Printing odd number if(i % 2 === 1)
