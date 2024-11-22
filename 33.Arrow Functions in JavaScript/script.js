b8722deaad17f666fa6fb5c704a38ad02575a222
RaviGahire <ravigahire3@gmail.com>
2024-11-22 13:18:40 +0530

    Arrow function

---
+++ 33.Arrow Functions in JavaScript/script.js
@@ -0,0 +1,33 @@
+// How we declared function
+
+// frist function declaration
+ function square (num){
+    return num * num
+ }
+
+ console.log('This is a function declarion example',square(5));
+
+ // second function expression
+// this function cant be hoisted as normal function we cant call this function befor declartion.
+ const add = function (num){
+    return num + num
+
+ }
+
+ console.log('This is a function expression example',add(5));
+
+
+ // Arrow function
+
+ let sub = (num1,num2) => { return num1 - num2}
+
+ console.log('This is a function arrow example',sub(5,10));
+
+ // feauter of arrow function wthout using return key word.
+ //This actaully call a implicite return 
+ // implicite return means automatic return
+
+ let firstFunc = (num) => num * num // this also work same as the above arrow function we not use return key word here.
+ let secondFunc = num => num * num // if there is one parameter so we not use the brackets. we use barcket for more then one parameter.
+ 
+
