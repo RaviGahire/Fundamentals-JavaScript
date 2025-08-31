fe613b8010bc01276455ab064c3924d67f751534
RaviGahire <ravigahire3@gmail.com>
2025-08-31 17:46:36 +0530

    Template literals

---
+++ 6.Template Literals String Methods Properties/tempalte.js
@@ -0,0 +1,15 @@
+
+
+const str = 'Hello my name ravi'
+
+const js = 'and Recat developer'
+
+function a (i){
+    return i  
+}
+
+const newi= a(js)
+
+let newstr = `${str} and i am js developer ${newi}`
+
+console.log(newstr);
