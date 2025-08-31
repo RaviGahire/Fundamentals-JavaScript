3b2bfe7d0c394ac06fbe3bd2b4fe81b626c6bc47
RaviGahire <ravigahire3@gmail.com>
2025-08-31 18:53:06 +0530

    How arrow function behave with objects

--- 33.Arrow Functions in JavaScript/script.js
+++ 33.Arrow Functions in JavaScript/script.js
@@ -25,9 +25,15 @@
 
  // feauter of arrow function wthout using return key word.
  //This actaully call a implicite return 
- // implicite return means automatic return
+ // implicit return means automatic return
 
  let firstFunc = (num) => num * num // this also work same as the above arrow function we not use return key word here.
  let secondFunc = num => num * num // if there is one parameter so we not use the brackets. we use barcket for more then one parameter.
  
 
+// How its work with objects let see
+// const obj2 = ()=>  {Name:'ravi'} we cannot return object like this we have to use following two methods 
+const obj1 = ()=> {return {Name:'ravi'}}
+const obj2 = ()=> ( {Name:'ravi'})
+console.log(obj1())
+console.log(obj1())
